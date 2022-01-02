c21dfe43edc2722b69c478751e2c565d27a38a0d
Sanjeev024 <sanjeevdubey34@gmail.com>
2022-01-02 14:00:31 +0530

    Created App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h3>Build Sign Up & Login UI in React</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
